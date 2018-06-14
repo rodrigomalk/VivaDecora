11e603de206712ce14340c645ada52353f3f4fc0
Rodrigo Teixeira <contato@rodrigomalk.com.br>
2018-06-14 15:17:48 -0300

    Update project_files.py
    Organização de código

--- scrape/project_files.py
+++ scrape/project_files.py
@@ -1,10 +1,10 @@
 
-from link import Link
-from pprint import pprint
+from scrape.link import Link
+
 
 class ProjectFiles(object):
 
-    def __init__(self, url, domain = None, scrapper = None):
+    def __init__(self, url, domain=None, scrapper=None):
 
         if not url.startswith('/'):
             url = "/" + url
@@ -12,44 +12,14 @@ class ProjectFiles(object):
         self.domain = domain
         self.scrapper = scrapper()
         self.files = []
-        self.files_ordened = []
 
-    def list_files(self, url = None, parent = None):
+    def list_files(self, url=None, parent=None):
         if url is None:
             url = self.url
 
         for link in self.scrapper.get_links(self.domain + url):
-            link_file = Link(self.domain + link, parent, scrapper = self.scrapper)
+            link_file = Link(self.domain + link, parent, scrapper=self.scrapper)
             self.files.append(link_file)
             if link_file.is_folder():
-                yield from self.list_files (link, parent = link_file)
+                yield from self.list_files (link, parent=link_file)
             yield link_file
-
-    def list(self):
-        for file in self.files:
-            file.get_depth()
-            if file.is_folder():
-                self.files_ordened.append(file.get_name())
-                self.deepening_list(file, 1)
-            elif file.get_depth() == 0:
-                self.files_ordened.append(file.get_name())
-        return self.files_ordened
-
-    def deepening_list(self, parent, depth):
-        for file in self.files:
-            name = ''
-            cont = depth
-            if file.parent == parent:
-                while cont > 0:
-                    name += '    '
-                    cont -= 1
-                name += '|_...' + file.get_name()
-                self.files_ordened.append(name)
-                if file.is_folder():
-                    depth += 1
-                    self.files.remove(file)
-                    self.deepening_list(file, depth)
-
-    def print_tree(self):
-        self.list()
-        pprint(self.files_ordened)

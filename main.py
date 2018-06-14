163469347a1a3539499373947584c2a0fb656ed6
Rodrigo Teixeira <contato@rodrigomalk.com.br>
2018-06-13 23:16:37 -0300

    Update main.py
    Tentativa de fazer a árvore sem os loopings

--- main.py
+++ main.py
@@ -14,28 +14,30 @@ def main():
         print('[{projeto}]'.format(projeto=project))
 
         project_files = ProjectFiles(project, domain = DOMAIN, scrapper = GitHubScrapper)
-        files = list(project_files.list_files())
+        files = reversed(list(project_files.list_files()))
         result = dict()
         all_lines = 0
         all_bytes = 0
 
         for project_file in files:
-            if project_file.extension not in result:
-                result[project_file.extension] = {
-                    'lines': 0,
-                    'lines_percent': 0.0,
-                    'bytes': 0.0,
-                    'bytes_percent': 0.0
-                }
-            all_lines += project_file.get_lines()
-            all_bytes += project_file.get_bytes()
-            result[project_file.extension]['lines'] += project_file.get_lines()
-            result[project_file.extension]['bytes'] += project_file.get_bytes()
+            if not project_file.is_folder():
+                if project_file.extension not in result:
+                    result[project_file.extension] = {
+                        'lines': 0,
+                        'lines_percent': 0.0,
+                        'bytes': 0.0,
+                        'bytes_percent': 0.0
+                    }
+                all_lines += project_file.get_lines()
+                all_bytes += project_file.get_bytes()
+                result[project_file.extension]['lines'] += project_file.get_lines()
+                result[project_file.extension]['bytes'] += project_file.get_bytes()
         for project_file in files:
-            result[project_file.extension]['lines_percent'] = '(' + str(round(100 * result[project_file.extension]['lines'] / all_lines, 2)) + '%)'
-            result[project_file.extension]['bytes_percent'] = '(' + str(round(100 * result[project_file.extension]['bytes'] / all_bytes, 2)) + '%)'
+            if not project_file.is_folder():
+                result[project_file.extension]['lines_percent'] = '(' + str(round(100 * result[project_file.extension]['lines'] / all_lines, 2)) + '%)'
+                result[project_file.extension]['bytes_percent'] = '(' + str(round(100 * result[project_file.extension]['bytes'] / all_bytes, 2)) + '%)'
         pprint(result)
-        project_files.print_tree()
+        pprint('\t' * project_file.get_depth() + project_file.get_name())
 
     print('[FIM.{_date:%Y/%m/%d %H:%M:%S}]'.format(_date=datetime.now()))
 

3494613ac1486bf131e2b0fdcbd19dcc79346099
Rodrigo Teixeira <contato@rodrigomalk.com.br>
2018-06-13 21:21:09 -0300

    Update project_files.py
    Inserção do código para a árvore

--- project_files.py
+++ project_files.py
@@ -1,5 +1,6 @@
 
 from link import*
+from pprint import pprint
 
 class ProjectFiles(object):
 
@@ -11,6 +12,7 @@ class ProjectFiles(object):
         self.domain = domain
         self.scrapper = scrapper()
         self.files = []
+        self.files_ordened = []
 
     def list_files(self, url = None, parent = None):
         if url is None:
@@ -18,7 +20,31 @@ class ProjectFiles(object):
 
         for link in self.scrapper.get_links(self.domain + url):
             link_file = Link(self.domain + link, parent, scrapper = self.scrapper)
+            self.files.append(link_file)
             if link_file.is_folder():
                 yield from self.list_files (link, parent = link_file)
             else:
                 yield link_file
+
+    def list(self):
+        for file in self.files:
+            file.get_depth()
+            if file.is_folder():
+                self.files_ordened.append(file.get_name())
+                self.files.remove(file)
+                self.deepening_list(file)
+        return self.files_ordened
+
+    def deepening_list(self, parent):
+        for file in self.files:
+            if file.parent == parent:
+                self.files_ordened.append(file.get_name())
+                self.files.remove(file)
+                if file.is_folder():
+                    self.files_ordened.append(file.get_name())
+                    self.files.remove(file)
+                    self.deepening_list(file)
+
+    def print_tree(self):
+        self.list()
+        pprint(self.files_ordened)
